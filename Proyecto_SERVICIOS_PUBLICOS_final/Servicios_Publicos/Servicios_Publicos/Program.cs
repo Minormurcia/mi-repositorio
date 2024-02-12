cc8fe306aebe16a36ea66be8978c05e5139af5ad
unknown <minormurcia@gmail.com>
2024-02-12 11:38:48 -0600

    actualizado

--- Proyecto_SERVICIOS_PUBLICOS_final/Servicios_Publicos/Servicios_Publicos/Program.cs
+++ Proyecto_SERVICIOS_PUBLICOS_final/Servicios_Publicos/Servicios_Publicos/Program.cs
@@ -162,7 +162,7 @@ class Program
             Console.Clear();
 
             numeroPago = Enumerable.Repeat(0, tamano).ToArray<int>();  //repite y pone valores en los espacios en blanco
-            fecha = Enumerable.Repeat(DateTime.Parse("01/01/0001"), tamano).ToArray<DateTime>(); //  revisar por que no funciona
+            fecha = Enumerable.Repeat(DateTime.Parse("01/01/0001"), tamano).ToArray<DateTime>(); 
             hora = Enumerable.Repeat(TimeOnly.Parse("00:00"), tamano).ToArray<TimeOnly>();
             cedula = Enumerable.Repeat("", tamano).ToArray<string>();
             nombre = Enumerable.Repeat("", tamano).ToArray<string>();
@@ -277,7 +277,7 @@ class Program
 
                     Console.WriteLine("Ingrese el monto de pago cliente:  "); 
                     double PagoCliente = double.Parse(Console.ReadLine());                 
-                    if (PagoCliente < (mPagar - comision))
+                    if (PagoCliente < mPagar)
                     {
                         Console.WriteLine("El Pago del Cliente es menor que el monto adeudado");
                         Console.ReadKey();
@@ -285,7 +285,7 @@ class Program
                     }
                     montoPagaCliente[posicion] = PagoCliente;
                                         
-                    double vueltoCliente = PagoCliente - (mPagar - comision);
+                    double vueltoCliente = PagoCliente - mPagar;
                     vuelto[posicion] = vueltoCliente;
                     Console.WriteLine("El vuelto es de:  " + vueltoCliente);
 
@@ -510,7 +510,7 @@ class Program
 
                 Console.WriteLine("Ingrese el monto de pago cliente:  ");
                 double PagoCliente = double.Parse(Console.ReadLine());
-                if (PagoCliente < (mPagar - comision))
+                if (PagoCliente < mPagar)
                 {
                     Console.WriteLine("El Pago del Cliente es menor que el monto adeudado");
                     Console.ReadKey();
@@ -518,7 +518,7 @@ class Program
                 }
                 montoPagaCliente[posVector] = PagoCliente;
 
-                double vueltoCliente = PagoCliente - (mPagar - comision);
+                double vueltoCliente = PagoCliente - mPagar;
                 vuelto[posVector] = vueltoCliente;
                 Console.WriteLine("El vuelto es de:  " + vueltoCliente);
 
--- Proyecto_SERVICIOS_PUBLICOS_final/Servicios_Publicos/Servicios_Publicos/obj/Debug/net8.0/Servicios_Publicos.AssemblyInfo.cs
+++ Proyecto_SERVICIOS_PUBLICOS_final/Servicios_Publicos/Servicios_Publicos/obj/Debug/net8.0/Servicios_Publicos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Servicios_Publicos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4452a13616ce0efcc52dff68b81f68ecb120e458")]
 [assembly: System.Reflection.AssemblyProductAttribute("Servicios_Publicos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Servicios_Publicos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
